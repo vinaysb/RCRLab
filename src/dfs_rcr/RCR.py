10c43de5b7bcb714659ae321d5cec3bbf24ad7b6
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2020-02-20 10:18:09 +0100

    Added comments

--- src/dfs_rcr/RCR.py
+++ src/dfs_rcr/RCR.py
@@ -25,20 +25,25 @@ def do_rcr(
     :return: Concordance values for each hyp which are identified by their respective upstream node.
     """
 
+    # Generate the knowledge assembly model
     kam = generate_kam(kam_path)
 
+    # Overlay the data on the KAM
     # mixed_model = overlay_data(kam)
     mixed_model = overlay_random_data(kam)
 
     results = []
 
+    # For all hyp's calculate the point probability
     for upstream, hyp in hyp_generator(mixed_model):
 
         if hyp:
-            results.append((upstream, calculate_probability(mixed_model, hyp, upstream)))
+            results.append((upstream, calculate_point_probability(mixed_model, hyp, upstream)))
 
+    # Adjust the p values/probability with the help of family-wise error correction
     adjusted_pvals = list(multipletests([result[1] for result in results], method=method)[1])
 
+    # Return the result as a list of tuple containing the upstream node and its adjusted p value
     return [(result[0], pval) for result, pval in zip(results, adjusted_pvals)]
 
 
@@ -52,14 +57,19 @@ def generate_kam(
     :return: KAM graph as a NetworkX DiGraph.
     """
 
+    # Read the file containing the kam file
     kam_df = pd.read_csv(kam_path, sep='\t', header=None)
+
+    # Rename the column headers are Source, Relationship and Target
     kam_df.columns = ['Source', 'Relationship', 'Target']
 
+    # Map relationships between the nodes as either +1 or -1 based on the interaction
     rlsp_mapping = {
         'activates': 1,
         'inhibits': -1
     }
 
+    # Add the data to a directed graph
     kam = nx.DiGraph()
 
     for edge in kam_df.index:
@@ -82,6 +92,7 @@ def hyp_generator(
     """
 
     for node in mixed_model.nodes:
+        # Split the generated dfs into a list of lists based on the node
         yield (node, _list_split(list(nx.edge_dfs(mixed_model, node)), node))
 
 
@@ -109,6 +120,7 @@ def overlay_random_data(
     """
 
     for node in kam.nodes:
+        # For all chemicals the node attribute is ambiguous
         if node.startswith('CHEBI'):
             kam.nodes[node]["regulation"] = 0
         else:
@@ -117,7 +129,7 @@ def overlay_random_data(
     return kam
 
 
-def calculate_probability(
+def calculate_point_probability(
         graph: nx.DiGraph,
         hyp: List[List[Any]],
         upstream: Any
@@ -129,23 +141,36 @@ def calculate_probability(
     :param upstream: Upstream node of the hypothesis.
     :return: The probability.
     """
+
+    # Keep a track of the number of positive and ambiguous nodes
     num_of_positives = 0
     num_of_ambiguous = 0
 
+    # Loop over every branch in the hyp
     for branch in hyp:
-        upstream_relation = graph.nodes[upstream]['regulation']
-        effective_relation = upstream_relation
+        # Remember the attribute of the upstream node
+        upstream_attr = graph.nodes[upstream]['regulation']
+
+        # Initialize the effective attribute as the upstream node attribute
+        effective_attr = upstream_attr
 
         for source, target in branch:
+            # Check if the node is ambiguous
             if graph.nodes[target]['regulation'] == 0:
                 num_of_ambiguous += 1
 
-            effective_relation *= graph.get_edge_data(source, target)['effect'] * graph.nodes[target]['regulation']
+            # Multiply the node and the edge values to get the effective attribute value
+            effective_attr *= graph.get_edge_data(source, target)['effect'] * graph.nodes[target]['regulation']
 
-            if effective_relation == upstream_relation:
+            # If after each node pass the effective attribute is equivalent to the upstream attribute then it is a
+            # positive hit for that node.
+            if effective_attr == upstream_attr:
                 num_of_positives += 1
 
+    # Flatten the hyp
     flat_hyp = set([node for branch in hyp for node in branch])
+
+    # Calculate the pmf of the binomial distribution given the probability of achieving the result is 0.5
     return binom.pmf(num_of_positives, len(flat_hyp) - num_of_ambiguous, 0.5)
 
 
